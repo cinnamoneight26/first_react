810ae1944e4df389307dfdf0b5ea2c6b46d88358
cinnamoneight26 <cinnamoneight26@gmail.com>
2021-12-02 09:07:01 +0900

    최근 검색어 / 추천 검색어 추가

---
+++ js/views/TabView.js
@@ -0,0 +1,43 @@
+import { qs } from "../helpers.js";
+import View from "./View.js";
+
+const TabType = {
+    KEYWORD : 'KEYWORD',
+    HISTORY : 'HISTORY'
+};
+
+const TabLable = {
+    [TabType.KEYWORD] : '추천 검색어',
+    [TabType.HISTORY] : '최근 검색어'
+}
+export default class TabView extends View {
+    constructor() {
+        super(qs('#tab-view'));
+        this.template = new Template();
+    }
+
+    show() {
+        this.element.innerHTML = this.template.getTabList();
+        super.show();
+    }
+}
+
+class Template {
+    getTabList() {
+        return `
+        <ul class="tabs">
+            ${Object.values(TabType)
+                .map(tabType => ({ tabType, tabLable : TabLable[tabType] }))
+                .map(this._getTab)
+                .join("")
+            }
+        </ul>`
+    }
+
+    _getTab( { tabType, tabLable } ){
+        return `
+        <li data-tab="${tabType}">
+            ${tabLable}
+        </li>`
+    }
+}

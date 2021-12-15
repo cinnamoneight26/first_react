c217694a1dab8a313b8f54d123a066db6a88321a
cinnamoneight26 <cinnamon.eight26@gmail.com>
2021-12-16 08:55:00 +0900

    input 추가 / 하나의 루트 노드 / Fragment

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -4,9 +4,17 @@ const element =  (
     // JSX의 모양은 HTML이지만 실제로는 자바스크립트이기 때문에 사용방법이 조금다름
     // 어트리뷰트 이름도 그 중 하나인데 html은 소문자만 사용하지만 JSX는 카멜케이스를 사용한다.
     // 자바스크립트에서 class는 예약어 중 하나이기 때문에 jsx에서는 className이라는 속성을 사용한다.
-    <header>
-        <h2 className="container">검색</h2>
-    </header>
+    <>
+        <header>
+            <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+            <form>
+                <input type="text" placeholder="검색어를 입력하세요." autoFocus />
+                <button type="reset" className="btn-reset"></button>
+            </form>
+        </div>
+    </>
 );
 
 ReactDOM.render(element, document.querySelector('#app'));

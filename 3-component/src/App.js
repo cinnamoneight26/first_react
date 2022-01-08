5f18af73d279edaf72d3b4c88edcb61f1df9b314
cinnamoneight26 <cinnamon.eight26@gmail.com>
2022-01-08 16:00:40 +0900

    함수형 컴포넌트를 재활용할 수 있는 형태로 개선 props

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 // Header 함수형 컴포넌트 import
 import Header from "./components/Header.js";
 
-
 export default class App extends React.Component {
   render() {
-    return <Header />
+    return (
+    <>
+      <Header title="검색" /> 
+      <Header title="프로필" /> 
+      <Header title="상품" />
+    </>
+    );
   }
 }

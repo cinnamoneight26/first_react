e81cb4771f02bde6f47ce0c82d88b2b45b19150a
cinnamoneight26 <cinnamon.eight26@gmail.com>
2022-01-08 16:27:04 +0900

    searchForm 추가 / 버튼 추가

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 // Header 함수형 컴포넌트 import
 import Header from "./components/Header.js";
+// SearchForm 컴포넌트 추가
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
   render() {
     return (
     <>
       <Header title="검색" /> 
-      <Header title="프로필" /> 
-      <Header title="상품" />
+      {/* <Header title="프로필" /> 
+      <Header title="상품" /> */}
+      <div className="container">
+        <SearchForm />
+      </div>
+
     </>
     );
   }
---
+++ 3-component/src/components/SearchForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// 이 컴포넌트를 외부에서 모듈로 등록하여 쓸 수 있도록 expert default 기본 모듈로 설정
+export default class SearchForm extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            searchKeyword : ""
+        };
+    }
+
+    handleChangeInput(event) {
+        const searchKeyword = event.target.value;
+        this.setState({ searchKeyword })
+    }
+
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="검색어를 입력하세요." 
+                    autoFocus 
+                    value={this.state.searchKeyword}
+                    onChange={event => this.handleChangeInput(event)}
+                />
+                {this.state.searchKeyword.length > 0 &&
+                    <button type="reset" className="btn-reset"></button> 
+                }
+            </form>
+        )
+    }
+}

d1729c78a3dacead2b1fe6620e3f3a8cfcf30fa9
cinnamoneight26 <cinnamon.eight26@gmail.com>
2022-01-16 20:10:20 +0900

    컴포넌트 조합 - 추천 검색어만 수정한 상태

--- 3-component/src/components/KeywordList.js
+++ 3-component/src/components/KeywordList.js
@@ -2,20 +2,33 @@ import React from "react";
 import List from "./List.js";
 import store from "../Store.js";
 
-export default class KeywordList extends List {
-    componentDidMount() {
-        const data = store.getKeywordList();
-        this.setState({
-            data,
-        });
+export default class KeywordList extends React.Component {
+    constructor() {
+        super() 
+
+        this.state = {
+            KeywordList: [],
+        }
     }
 
-    renderItem(item, index) {
+    componentDidMount() {
+        const KeywordList = store.getKeywordList();
+        this.setState( { KeywordList } );
+    }
+
+    render() {
         return (
-            <>
-                <span className="number">{index + 1}</span>
-                <span>{item.keyword}</span>
-            </>
+            <List 
+                data={this.state.KeywordList} 
+                onClick={this.props.onClick} 
+                renderItem={(item, index) => {
+                    return (
+                        <>
+                            <span className="number">{index + 1}</span>
+                            <span>{item.keyword}</span>
+                        </>
+                    )
+                }}/>
         )
     }
 }
